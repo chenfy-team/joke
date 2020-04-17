11e2d1e18f577f60ba9d846803ddceb39be48974
chenfycc <63218454+chenfycc@users.noreply.github.com>
2020-04-17 11:31:51 +0800

    Add files via upload

---
+++ 老营销号了.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+string a,b,c;
+int main()
+{
+	cout<<"主体:";
+	cin>>a;
+	cout<<"说法:";
+	cin>>b;
+	cout<<"另一种说法:";
+	cin>>c;
+	system("cls");
+	cout<<"    "<<a<<b<<"是怎么回事呢？"<<a<<"相信大家都很熟悉，但是"<<a<<b<<"是怎么回事呢，下面就让小编带大家一起了解吧。"<<endl<<"    "<<a<<b<<",其实就是"<<c<<"大家可能会很惊讶"<<a<<"怎么会"<<b<<"呢？但事实就是这样，小编也感到非常惊讶。"<<endl<<"    "<<"这就是关于"<<a<<b<<"的事情了，大家有什么想法呢，欢迎在评论区告诉小编哦！"<<endl;
+	system("pause>nul"); 
+}
